99769ddeb475329d0f8db43122627b8331acd75f
rajaaaaaaax <premwairagade001@gmail.com>
2024-09-03 04:04:42 -0700

    fix play button

--- src/app/watch/[type]/[id]/page.tsx
+++ src/app/watch/[type]/[id]/page.tsx
@@ -117,7 +117,7 @@ const page = async ({ params }: { params: { id: string; type: string } }) => {
           </div>
           <Options />
         </div>
-        <div className="flex flex-col justify-end items-end flex-1 h-[500px] z-20 mr-5">
+        <div className="flex flex-col justify-end items-end flex-1 h-[500px] z-20">
           <PlayButton
             getSeasonList={getSeasonList}
             imdbId={data?.externalIds?.imdb_id}
